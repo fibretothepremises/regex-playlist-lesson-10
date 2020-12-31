83b49b6fd92dffa92ca34f34f7947313e75be2d3
Fibre P <fttx5525@gmail.com>
2020-12-31 16:36:47 +1100

    Add regex and validity testing functions. Add styles dependent on validity of form input fields

--- validation.js
+++ validation.js
@@ -1,1 +1,25 @@
 // validation script here
+const inputs = document.querySelectorAll('input')
+const patterns = {
+    username: /^[a-z\d]{5,12}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]+)(\.[a-z]+)?$/i,
+    password: /^[\w@-]{8,20}$/i,
+    telephone: /^\d{11}$/,
+    slug: /^[a-z\d-]{8,20}$/
+}
+
+// validation function
+function validate(field,regex){
+    if(regex.test(field.value)){
+        field.className='valid';
+    } else {
+        field.className='invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup',(e)=> {
+        // console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value])
+    })
+})
